f3e8e4bec521b605d86faa8a85a20ed0bec32a6d
sdvgeek2 <pamelaschulz8@gmail.com>
2022-07-21 13:14:03 +0200

    Add project files.

---
+++ ImageTextractor/View/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageTextractor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files(*.png; *.jpeg)| *.png; *.jpeg | All files(*.*) | *.* ";
+
+            if (dialog.ShowDialog() == true)
+            {
+                ChosenImage.Source = new BitmapImage(new Uri(dialog.FileName));
+            }
+        }
+    }
+}
